060188bbf1cd36cf007dba5e1387557e058270df
Xanjit Xarkar <ssanjit856@gmail.com>
2021-07-13 14:13:26 +0530

    enabled python script running functionality

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ const { spawn } = require('child_process');
 const open = require('open')
 const app = express()
 const port = 5000
+function download(name) {
+  if (name && name !=='') {
+    var link = document.createElement('a');
+    link.download = name;
+    link.href ="file.png";  
+    link.click();
+  }
+}
+
 app.get('/login', (req, res) => {
-    const { email, password } = req.query
-    console.log(email,password);
+    const { email, password, url } = req.query
 var dataToSend;
     try {
-        const python = spawn('python', ['./scripts/main.py', email,password]);
+        const python = spawn('python', ['./scripts/main.py', email,password,url]);
         python.stdout.on('data', (data) => {
             dataToSend = data.toString();
         });
         python.on('close', (_) => {
-            open( dataToSend, function (err) {
-  if ( err ) throw err;    
-});
-            res.send("Downloading")
+            res.send(dataToSend)
+//             open(dataToSend, function (err) {
+//                 res.send("Downloading:")
+//   if ( err ) throw err;    
+// });
+            
 
         });
     }
--- scripts/main.py
+++ scripts/main.py
@@ -2,11 +2,13 @@ import sys
 from mega.mega import Mega
 mega = Mega()
 try:
-    m = mega.login(sys.argv[1], sys.argv[2])
-    url = m.download_url(
-        "https://mega.nz/file/bXwRlICb#U2HdGT-JQS1AGtPcz8uAynajK4QPN-JucxYTpIaPv4I")
-    print(url.split())
+    email, password, url = sys.argv[1], sys.argv[2], sys.argv[3]
+    m = mega.login(email, password)
+    url_info = m.get_public_url_info(sys.argv[3])
+    download_url = m.download_url(sys.argv[3])
+    print([url_info, download_url])
+    # print(f"{email} {password} {url} {download_url}")
 
 except Exception as e:
     pass
-    # print(f"Error logging into your mega account: {e}")
+    print(f"Error logging into your mega account: {e}")
